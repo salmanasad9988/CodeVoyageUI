8618590cae0d16ea0d2f481ca2bcf3ca295a8ac0
Salman Asad <salmanasad9988@gmail.com>
2023-10-18 04:09:33 -0700

    Categories Column

--- src/app/features/blog-post/models/blog-post.model.ts
+++ src/app/features/blog-post/models/blog-post.model.ts
@@ -1,3 +1,5 @@
+import { Category } from "../../category/models/category.model";
+
 export interface BlogPost {
     id: string;
     title: string;
@@ -7,4 +9,5 @@ export interface BlogPost {
     author: string;
     publishedDate: Date;
     isPublic: boolean;
+    categories: Category[];
 }
