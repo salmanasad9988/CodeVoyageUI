ddcc69b8b6055f908dfd399f0d9391c4ec7cd277
Salman Asad <salmanasad9988@gmail.com>
2023-10-20 02:22:23 -0700

    multi select control on edit blogpost component

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -14,13 +16,19 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   paramsSubscription?: Subscription;
   blogPost?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
 
   constructor(private route: ActivatedRoute, 
     private blogPostService: BlogPostService, 
+    private categoryService: CategoryService,
     private router: Router)
   {}
   
   ngOnInit(): void {
+
+    this.categories$ = this.categoryService.getAllCategories();
+
     this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
@@ -30,11 +38,16 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
           .subscribe({
             next: (response) => {
               this.blogPost = response;
+              this.selectedCategories = response.categories.map(x => x.id);
             }
           });
         }
       }
     });
+  }
+
+  onFormSubmit(): void {
+
   }
 
   ngOnDestroy(): void {
